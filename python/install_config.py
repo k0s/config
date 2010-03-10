793bab3ab7f3c265bc935b0c8da6c7a18eb9aa84
Jeff Hammel <k0scist@gmail.com>
2010-03-10 14:12:41 -0500

    install smartopen by default and some other cleanup (untested)

--- python/install_config.py
+++ python/install_config.py
@@ -14,9 +14,12 @@ os.chdir(HOME)
 # make the current directory a repository
 import subprocess
 
+
+
 commands = [ ['hg', 'init'],
              ['hg', 'pull', SRC],
              ['hg', 'update', '-C'],
+             ['hg', 'clone', 'http://bitbucket.org/ianb/virtualenv'],
 
              # site-specific files
              ['mkdir', '-p', '.subversion'],
@@ -24,10 +27,24 @@ commands = [ ['hg', 'init'],
              ['ln', '-s', os.path.join(HOME, '.subversion_config/config'), os.path.join(HOME, '.subversion/config')],
              ]
 
-for command in commands:
-    code = subprocess.call(command)
-    if code:
-        sys.exit(code)
+def execute(*commands):
+    for command in commands:
+        print ' '.join(command)
+        code = subprocess.call(command)
+        if code:
+            sys.exit(code)
 
+execute(*commands)
 
+def install_develop(package):
+    src = 'http://k0s.org/hg/%s' % package
+    commands = [ ['virtualenv/virtualenv.py', package],
+                 ['mkdir', '%s/src'],
+                 ['hg', 'clone', src, '%s/src/%s' % (package, package)],
+                 ['%s/bin/python', '%s/src/%s/setup.py', 'develop'] ]
+    execute(*commands)
 
+# install some python
+install_develop('smartopen')
+
+postinstall_commands = [ ['ln', '-s', os.path.join(HOME, 'smartopen', 'bin', 'smartopen'), os.path.join(HOME, 'bin', 'smartopen')]
