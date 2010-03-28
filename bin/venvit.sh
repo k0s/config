1042973ecf6c406d0fae8a7132290643af0b329d
Jeff Hammel <k0scist@gmail.com>
2010-03-28 16:38:51 -0400

    allow cloning from hg instead of just svn

--- bin/venvit.sh
+++ bin/venvit.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
+# make a virtualenv for the product
 
+# usage
 if (( $# != 1 ))
 then
-    echo "Usage: $0 <svn-location>"
+    echo "Usage: $0 <svn-or-hg-location>"
     exit 0
 fi
 
+# determine name of the package
 NAME=$1
-
 for i in /trunk /branches /tag
 do
     NAME=${NAME%%$i*}
 done
 NAME=${NAME##*/}
 
-#echo $NAME
+if svn info $1 2> /dev/null
+then
+    CHECKOUT="svn co"
+else
+    CHECKOUT="hg clone"
+fi
 
+# create a virtualenv and install the software
 VIRTUAL_ENV_LOCATION="${HOME}/virtualenv/virtualenv.py"
-
 python ${VIRTUAL_ENV_LOCATION} ${NAME}
 cd ${NAME}
 source bin/activate
 mkdir src/
 cd src/
-svn co $1 ${NAME}
+$CHECKOUT $1 ${NAME}
 cd ${NAME}
 python setup.py develop
