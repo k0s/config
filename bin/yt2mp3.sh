b91c68cf60298c2fe9232d95dfdfcb429ff9210b
Jeff Hammel <jhammel@mozilla.com>
2010-05-27 22:10:34 -0700

    more sanity in this script for debugging

--- bin/yt2mp3.sh
+++ bin/yt2mp3.sh
@@ -4,9 +4,9 @@ YOUTUBEDL=/home/jhammel/music/youtube-dl
 
 URL=$1
 TITLE=$($YOUTUBEDL --get-title $URL)
-$YOUTUBEDL -b -i -r 50k $URL -o "%(title)s.%(ext)s" 
 EXT=mp4
+$YOUTUBEDL -b -i -r 50k $URL -o "%(title)s.${EXT}" 
 rm -f audiodump.wav
 mplayer -vc null -vo null -ao pcm "$TITLE.$EXT"
 lame -q 2 audiodump.wav "$TITLE.mp3"
-rm "$TITLE.$EXT"
+#rm "$TITLE.$EXT"
