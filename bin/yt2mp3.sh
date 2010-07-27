273e96bbc3e3a039577e789c7981d892ef32ba16
Jeff Hammel <jhammel@mozilla.com>
2010-07-26 19:12:47 -0700

    modification for new youtube-dl

--- bin/yt2mp3.sh
+++ bin/yt2mp3.sh
@@ -5,8 +5,8 @@ YOUTUBEDL=/home/jhammel/music/youtube-dl
 URL=$1
 TITLE=$($YOUTUBEDL --get-title $URL)
 EXT=mp4
-$YOUTUBEDL -b -i -r 50k $URL -o "%(title)s.${EXT}" 
+$YOUTUBEDL -i -r 50k $URL -o "%(title)s.${EXT}" 
 rm -f audiodump.wav
-mplayer -vc null -vo null -ao pcm "$TITLE.$EXT"
+mplayer -vo null -ao pcm "$TITLE.$EXT"
 lame -q 2 audiodump.wav "$TITLE.mp3"
 #rm "$TITLE.$EXT"
