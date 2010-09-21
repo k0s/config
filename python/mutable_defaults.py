5873cea83ff6ac98cd03056a43b888412321cf6b
Jeff Hammel <jhammel@mozilla.com>
2010-09-21 08:30:19 -0700

    add illustration program for mutable arguments, since this comes up a bunch

---
+++ python/mutable_defaults.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+class Foo(object):
+  def __init__(self, mutable=['default']):
+    self.foo = mutable
+    self.foo.append(1)
+
+if __name__ == '__main__':
+  bar = Foo()
+  print len(bar.foo)
+  fleem = Foo()
+  print len(fleem.foo)
+  assert len(fleem.foo) == 2, "Don't use default mutable arguments!"
