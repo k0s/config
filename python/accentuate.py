0c175043088d5533c5b4b32be17e1d76c56d2fe9
Jeff Hammel <jhammel@mozilla.com>
2011-03-04 11:43:15 -0800

    new program that does silly things

---
+++ python/accentuate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+mapping = {'a': ['Ȁ', 'ȁ'],
+           'c': ['ç'],
+           'e': ['ȅ'],
+           'n': ['И'], 
+           'o': ['ổ', 'ȍ'],
+           't': ['Ṱ'],
+           }
+
+if __name__ == '__main__':
+    import sys
+    arg = ' '.join(sys.argv[1:])
+    retval = []
+    for letter in arg:
+        if letter.lower() in mapping:
+            retval.append(random.sample(mapping[letter.lower()], 1)[0])
+        else:
+            retval.append(letter)
+    print ''.join(retval)
