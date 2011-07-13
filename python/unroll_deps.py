feff2f81aae29ee35d6c372374e44e97b31a7090
Jeff Hammel <jhammel@mozilla.com>
2011-07-12 17:25:21 -0700

    fixes; doesnt quite work

--- python/unroll_deps.py
+++ python/unroll_deps.py
@@ -4,6 +4,7 @@ def unroll_dependencies(dependencies):
     """unroll dependencies"""
     order = []
     for package, deps in dependencies.items():
+        print package, order
         try:
             index = order.index(package)
         except ValueError:
@@ -12,14 +13,14 @@ def unroll_dependencies(dependencies):
         for dep in deps:
             try:
                 dep_index = order.index(dep)
-                assert dep_index < index, "Cyclic dependencies detected"
+                assert dep_index < index, "Cyclic dependencies detected: %s, %s" % (package, dep)
             except ValueError:
                 order.insert(index, dep)
     return order
 
 if __name__ == '__main__':
     deps = {'packageA': set(['packageB', 'packageC']),
-            'packageB': set(['packageC', 'packageD']),
-            'packageC': set()}
+            'packageB': set(['packageC', 'packageD', 'packageE']),
+            'packageC': set(['packageE'])}
     unrolled = unroll_dependencies(deps)
     print unrolled
