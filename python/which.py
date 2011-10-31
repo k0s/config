223070fe9d0ed3c07709cfd36c98786b0191bb96
Jeff Hammel <jhammel@mozilla.com>
2011-10-31 10:48:57 -0700

    make which.py work

--- python/which.py
+++ python/which.py
@@ -1,12 +1,16 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
 def findInPath(fileName, path=os.environ['PATH']):
     """python equivalent of which; should really be in the stdlib"""
     dirs = path.split(os.pathsep)
     for dir in dirs:
         if os.path.isfile(os.path.join(dir, fileName)):
             return os.path.join(dir, fileName)
-        if mozinfo.isWin:
-            if os.path.isfile(os.path.join(dir, fileName + ".exe")):
-                return os.path.join(dir, fileName + ".exe")
+        if os.path.isfile(os.path.join(dir, fileName + ".exe")):
+            return os.path.join(dir, fileName + ".exe")
 
 if __name__ == '__main__':
     for i in sys.argv[1:]:
