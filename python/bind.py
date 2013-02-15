515aa92d3d3d3c6820b89fcaf1f67a9cba59db61
Jeff Hammel <jhammel@mozilla.com>
2013-02-15 15:18:31 -0800

    add a sample to bind to an instance, stub

---
+++ python/bind.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+illlustrate e.g. method bind for python
+"""
+
+class Foo(object):
+
+    @classmethod
+    def create(cls):
+        """create an instance and bind a method onto foo"""
+              
+
+    def foo(self):
+        print "Foo!"
+
