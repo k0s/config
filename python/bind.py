9ad86427700b0e1f4ada4dbb0ffe2eec7910ae73
Jeff Hammel <jhammel@mozilla.com>
2013-02-17 12:46:25 -0800

    stub this motha

--- python/bind.py
+++ python/bind.py
@@ -9,8 +9,20 @@ class Foo(object):
     @classmethod
     def create(cls):
         """create an instance and bind a method onto foo"""
-              
+        class decorator(object):
+            def __init__(self, function):
+                self.function = function
+            def __call__(self):
+                print "Bar!"
+                return self.function()
+
+        instance = cls()
+        instance.foo = decorator(instance.foo)
+        return instance
 
     def foo(self):
         print "Foo!"
 
+if __name__ == '__main__':
+    foo = Foo.create()
+    foo.foo()
