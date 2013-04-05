ea874ddc1c5e33f254f811b54df0e6386df03506
Jeff Hammel <jhammel@mozilla.com>
2013-04-04 19:18:01 -0700

    add a email-esque quoting script

---
+++ python/quote.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+quote text
+"""
+
+import sys
+import textwrap
+from subprocess import check_output as call
+
+def quote(text, prefix='> ', width=69):
+    """
+    returns quoted text
+    - prefix: string to prepend quote
+    - width: final width (emacs wraps at 70)
+    """
+    width -= len(prefix) # subtract the prefix
+    text = text.strip() # remove surrounding whitespace
+    lines = []
+    for line in text.splitlines():
+        line = line.strip()
+        lines.extend(textwrap.wrap(line, width))
+    return '\n'.join(['%s%s' % (prefix, line)
+                      for line in lines])
+
+def main(args=sys.argv[1:]):
+    text = sys.stdin.read()
+    sys.stdout.write(quote(text))
+
+if __name__ == '__main__':
+    main()
