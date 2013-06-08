501e7714995f4fe11c8fdf791a38499ac3fddca3
Jeff Hammel <jhammel@mozilla.com>
2013-06-08 08:26:37 -0700

    nearly works

--- bin/example/self-writing.sh
+++ bin/example/self-writing.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 # illustrate self-writing script (example)
+# This one does something hard and replaces dynamic data with sed.
+# Other solutions (magic markers, etc) are possible
 
-path=`readlink $0`
+path=`readlink -f $0`
 tmp=`tempfile`
+datestamp=`date`
+nonce="This script regenerated at "
+
+# avoiding -i for safety
+sed 's/\(echo \"'"${nonce}"'\).*\"/\1'"${datestamp}"'\"/' ${path} > ${tmp}
+
+echo "This script regenerated at "
