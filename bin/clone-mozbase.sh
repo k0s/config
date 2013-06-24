99a5a185b39ba23e731f85fb8bc0c904b93412c4
Jeff Hammel <jhammel@mozilla.com>
2013-06-24 11:16:35 -0700

    minor improvements

--- bin/clone-mozbase.sh
+++ bin/clone-mozbase.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+VENV=virtualenv.py
+
+if [[ -d "${VIRTUAL_ENV}" ]]
+then
+    echo "virtualenv: ${VIRTUAL_ENV}"
+fi
+
 git clone git@github.com:k0s/mozbase.git
 cd mozbase
 git remote add mozilla git@github.com:mozilla/mozbase.git
+git pull mozilla master
+git push origin master
