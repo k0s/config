fa07c11ceb796ea1b7dc1bf0a52623a741c3a762
Jeff Hammel <jhammel@mozilla.com>
2013-07-30 00:04:34 -0700

    make at least a pretend stub

--- python/hg-tricks.py
+++ python/hg-tricks.py
@@ -1,9 +1,20 @@
-# check status
-/       # check for outstanding changes
-        output = self._call(['st']).strip()
-        lines = [line for line in output.splitlines()
-                 if not line.startswith('?')]
-        if lines:
-            print "Outstanding changes:"
-            print output
-            raise AssertionError
+"""
+random collection of hg tricks in python
+"""
+
+# XXX STUB
+
+from subprocess import check_output as call
+
+### check status
+
+def changes(hg='hg')
+    """check for outstanding changes"""
+    output = call([hg, 'st']).strip()
+    lines = [line for line in output.splitlines()
+             if not line.startswith('?')]
+    if lines:
+        print "Outstanding changes:"
+        print output
+        return True
+
