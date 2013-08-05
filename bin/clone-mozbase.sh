a05df3832a0441440d1753dbb9f6352d5d51686e
Jeff Hammel <jhammel@mozilla.com>
2013-08-05 14:54:03 -0700

    creates a virtualenv now

--- bin/clone-mozbase.sh
+++ bin/clone-mozbase.sh
@@ -5,6 +5,24 @@ VENV=virtualenv.py
 if [[ -d "${VIRTUAL_ENV}" ]]
 then
     echo "virtualenv: ${VIRTUAL_ENV}"
+else
+    if [ -n "${VIRTUAL_ENV}" ]
+    then
+        echo "VIRTUAL_ENV defined but not a directory: ${VIRTUAL_ENV}"
+        exit 1
+    fi
+
+    if ! which ${VENV}
+    then
+        echo "${VENV} not found"
+        exit 1
+    fi
+    VIRTUAL_ENV=${PWD}/mozbase
+    echo "Creating virtualenv: ${VIRTUAL_ENV}"
+    ${VENV} ${VIRTUAL_ENV}
+    cd ${VIRTUAL_ENV}
+    . bin/activate
+    mkdir -p src
 fi
 
 git clone git@github.com:k0s/mozbase.git
