a510c38ed040c1a955d47118acb11610bfec2a7b
Jeff Hammel <jhammel@mozilla.com>
2013-08-13 23:07:44 -0700

    bin/setup-X.sh

--- bin/setup-X.sh
+++ bin/setup-X.sh
@@ -4,7 +4,7 @@
 for i in nm-applet gkrellm diodon # 'synapse -s'
 # To add: x-tile; qamixer (well, some mixer); gnome-activity journal
 do
-    if ! ps aux | grep ${i} | grep -v 'grep'
+    if ! pidof ${i}
     then
         echo "not running: $i"
         if which ${i}
