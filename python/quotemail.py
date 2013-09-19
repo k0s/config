10112f9aff4dc13b21c2cc2851d944d41548e1d2
Jeff Hammel <jhammel@mozilla.com>
2013-09-19 11:21:07 -0700

    python/quotemail.py

--- python/quotemail.py
+++ python/quotemail.py
@@ -3,6 +3,7 @@
 """
 quote as per email
 """
+# TODO -> textshaper
 
 def prefix(text, quote='> '):
     return '\n'.join(['%s%s' % (quote, line.rstrip())
@@ -10,4 +11,22 @@ def prefix(text, quote='> '):
 
 if __name__ == '__main__':
     import sys
-    print prefix(sys.stdin.read())
+    import optparse
+    usage = '%prog [options] [file] <file2> <...>'
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option('-i', '--in-place', dest='in_place',
+                      action='store_true', default=False,
+                      help="")
+    options, args = parser.parse_args()
+    if args:
+        for arg in args:
+            with file(arg) as f:
+                contents = f.read()
+            quoted = prefix(contents)
+            if options.in_place:
+                with file(arg, 'w') as f:
+                    f.write(quoted)
+            else:
+                print quoted
+    else:
+        print prefix(sys.stdin.read())
