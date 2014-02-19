e0fb10bb7488393bc036228ff279b9fd3f492ae2
Jeff Hammel <k0scist@gmail.com>
2014-02-18 20:09:42 -0800

    STUB: python/example/classes.py

--- python/example/classes.py
+++ python/example/classes.py
@@ -7,9 +7,13 @@ illustrate how classes work
 class A:
     pass
 
-tests = ["issubclass(A, A)"]
+tests = ["issubclass(A(), A)",
+         "issubclass(A, A)"]
 
 if __name__ == '__main__':
     for test in tests:
         print ("? {}".format(test))
-        print (eval(test))
+        try:
+            print (eval(test))
+        except Exception as e:
+            print ('{}'.format(repr(e)))
