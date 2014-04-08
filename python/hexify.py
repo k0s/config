5875b9e017c8ed447721acc9fac44d2863914e43
Jeff Hammel <k0scist@gmail.com>
2014-04-07 19:24:57 -0700

    make sneaky urls

--- python/hexify.py
+++ python/hexify.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python
+
 import sys
-print ''.join([ '%'+ hex(ord(i))[-2:] for i in ' '.join(sys.argv[1:]) ])
+
+def hexify(string, excludes=('/',)):
+    return ''.join([i if i in excludes else ('%'+ hex(ord(i))[-2:])
+                    for i in string])
+
+def hidden_url(string):
+    if '://' in string:
+        scheme, rest = string.split('://', 1)
+        if '/' in rest:
+            loc, rest = rest.split('/', 1)
+            return '{}://{}/{}'.format(scheme, loc, hexify(rest, excludes=('/',)))
+        else:
+            return string
+    else:
+        return hexify(string, excludes=('/',))
+
+def main(args=sys.argv[1:]):
+    string = ' '.join(args)
+    print hidden_url(string)
+
+if __name__ == '__main__':
+    main()
