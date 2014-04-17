7e34752b5761ee88248316baf7fc4e7cec689c83
Jeff Hammel <k0scist@gmail.com>
2014-04-16 17:02:42 -0700

    update

--- python/epoch2date.py
+++ python/epoch2date.py
@@ -9,12 +9,22 @@ import sys
 import time
 
 def main(args=sys.argv[1:]):
+    """CLI"""
 
+    # parse command line
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('seconds_since_epoch', help="seconds since epoch input")
+    parser.add_argument('seconds_since_epoch',
+                        type=float, nargs='?', default=time.time(),
+                        help="seconds since epoch input [DEFAULT: %(default)s]")
     options = parser.parse_args(args)
 
-    
+    # produce a datetime
+    dt = datetime.datetime.fromtimestamp(options.seconds_since_epoch)
+    dt2 = datetime.datetime.utcfromtimestamp(options.seconds_since_epoch)
+
+    # output
+    print (dt)
+    print (dt2)
 
 if __name__ == '__main__':
     main()
