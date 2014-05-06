05649dbdd1bb7a6582c909afc1d3da7bdef6dc14
Jeff Hammel <k0scist@gmail.com>
2014-05-06 15:18:40 -0700

    STUB: bin/example/pid.sh

--- bin/example/pid.sh
+++ bin/example/pid.sh
@@ -4,4 +4,13 @@ PID=$$
 
 echo "The PID of this shell script is ${PID}"
 ps auxwww | grep ${PID}
+
+if which tempfile
+then
+    PIDFILE=$(tempfile)
+    echo "PID file: ${PIDFILE}"
+    echo ${PID} > ${PIDFILE}
+    kill $(cat ${PIDFILE})
+fi
+
 kill ${PID}
