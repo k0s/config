b67ba92e4c45cf305643ab23cfbd9c4fafa7a098
Jeff Hammel <k0scist@gmail.com>
2016-01-04 12:56:15 -0800

    fixes

--- python/s5ify.py
+++ python/s5ify.py
@@ -18,12 +18,12 @@ def main(args=sys.argv[1:]):
 
     # parse command line
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_option('input')
-    options = parser.parse_parse(args)
+    parser.add_argument('input')
+    options = parser.parse_args(args)
 
     # sanity
     input = options.input
-    if not os.path.isfile(input)
+    if not os.path.isfile(input):
         parser.error("Not a file: '{}'".format(input))
     input = os.path.abspath(options.input)
 
