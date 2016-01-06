0b5a01307d6b21e113adea36bba0bf0024995d6e
Jeff Hammel <k0scist@gmail.com>
2016-01-06 09:44:57 -0800

    ch ch ch changes

--- python/s5ify.py
+++ python/s5ify.py
@@ -27,5 +27,19 @@ def main(args=sys.argv[1:]):
         parser.error("Not a file: '{}'".format(input))
     input = os.path.abspath(options.input)
 
+    # get output name from input
+    output_filename = os.path.splitext(os.path.basename(input))[0] + '.html'
+    output = os.path.join(os.path.dirname(input), output_filename)
+
+    # build command line
+    options = []
+    command = ['rst2s5.py']
+    command.extend(options)
+    command.extend([input, output])
+    print subprocess.list2cmdline(command)
+
+    # call it
+    subprocess.call(command)
+
 if __name__ == '__main__':
     main()
