84c23b63619453e98c0698599550cc3aaf338e22
Jeff Hammel <k0scist@gmail.com>
2016-08-24 14:39:51 -0700

    this now works again, and without docker-machine; ephermal testing for now, ephemeral testing forever

--- bin/docker_elasticsearch.sh
+++ bin/docker_elasticsearch.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
+# run an ephemeral elasticsearch
+
+# variables
+export IMAGE="elasticsearch"
+export PORT=9200
+
+# Run docker with a CID file so we can attach to it
 export CIDFILE=$(mktemp -u)
-docker run -P --cidfile ${CIDFILE} elasticsearch &
+docker run -P --cidfile "${CIDFILE}" "${IMAGE}" &
 sleep 10
 
+# ensure we can shutdown properly
 script_shutdown() {
     echo "**** Shutting down... ****"
    # http://unix.stackexchange.com/questions/55558/how-can-i-kill-and-wait-for-background-processes-to-finish-in-a-shell-script-whe
     docker kill `cat ${CIDFILE}`
 }
-
 trap 'script_shutdown' INT
 
-# TODO
-# export DOCKER_IP=$(docker-machine ip default)
-# export CONTAINER_PORT=$(docker port `cat ${CIDFILE}` | grep 9200 | sed 's/.*://')
-# export ELASTICSEARCH_URL="http://${DOCKER_IP}:${CONTAINER_PORT}/"
-# open ${ELASTICSEARCH_URL}
+# open the elasticsearch URL
+export ELASTICSEARCH_URL="http://$(docker port `cat "${CIDFILE}"` ${PORT})/"
+echo ${ELASTICSEARCH_URL}
+open "${ELASTICSEARCH_URL}"
+
 wait
 
