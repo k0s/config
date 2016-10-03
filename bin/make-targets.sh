4e1c0ab44c8479b32a96667e84876b863da77065
Jeff Hammel <k0scist@gmail.com>
2016-10-02 18:37:16 -0700

    correct

--- bin/make-targets.sh
+++ bin/make-targets.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # list makefile targets
-make -pn | grep '^ *[^#].*:.*' | sed 's/\([^:]+\):.*/\1/' | sort | uniq
+# from http://stackoverflow.com/questions/4219255/how-do-you-get-the-list-of-targets-in-a-makefile
+make -pRrq  2>/dev/null | awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($1 !~ "^[#.]") {print $1}}' | sort | egrep -v -e '^[^[:alnum:]]' -e '^$@$'
