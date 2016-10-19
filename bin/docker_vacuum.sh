47041aa9099b97ff2ce45d19ac16fbc565dbb1ee
Jeff Hammel <k0scist@gmail.com>
2016-10-19 09:04:34 -0700

    add docker cleanup helper

---
+++ bin/docker_vacuum.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# http://blog.yohanliyanage.com/2015/05/docker-clean-up-after-yourself/
+
+# Make sure exited containers are deleted
+docker rm -v $(docker ps -a -q -f status=exited)
+
+# Remove unwanted 'dangling' images
+docker rmi $(docker images -f "dangling=true" -q)
+
+# Remove unwanted volumes
+docker volume rm $(docker volume ls -qf dangling=true)
