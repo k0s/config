2ae542260cef555f910efa9eea91d83c2c23065d
Jeff Hammel <k0scist@gmail.com>
2016-10-28 18:54:56 -0700

    make into an almost real CLI

--- python/tld.py
+++ python/tld.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
+"""
+Top-Level Domain names (TLDs), country codes only
+"""
+
+import argparse
+import sys
+import urllib2
+
 URL = 'http://www.palosverdes.com/jesse/irc/country.txt'
 
-import urllib2
 
 def codes():
   f = urllib2.urlopen(URL)
@@ -16,11 +23,23 @@ def codes():
   return codes
 codes = codes()
 
+
 def get(code):
   code = code.lstrip('.').lower()
   return codes.get(code)
 
+def main(args=sys.argv[1:]):
+  """CLI"""
+
+  # parse command line
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument('tld', nargs='+',
+                      help="country-code TLD to look up")
+  options = parser.parse_args(args)
+
+  # lookup + output
+  for arg in options.tld:
+    print ('{}: {}'.format(arg, get(arg)))
+
 if __name__ == '__main__':
-  import sys
-  for arg in sys.argv[1:]:
-    print '%s: %s' % (arg, get(arg))
+  main()
