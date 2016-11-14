5f6879e42875cb8c35adc69a8e99a9c078b06dd8
Jeff Hammel <k0scist@gmail.com>
2016-11-13 16:02:30 -0800

    k8s is picky about how many dashes a deployment may have

--- bin/kurl.sh
+++ bin/kurl.sh
@@ -2,7 +2,7 @@
 
 # run curl within a k8s cluster via kubectl
 
-export KURL_DEPLOYMENT=curl-$(hostname)
+export KURL_DEPLOYMENT=curl-$(whoami)$(date +%s)
 kubectl run --rm ${KURL_DEPLOYMENT} --image=radial/busyboxplus:curl -i --tty
 
 
