b2ae317e1c9ab47b3c0c6107b039673b72bbbd1f
Jeff Hammel <k0scist@gmail.com>
2024-06-02 14:57:09 -0700

    install k0s.org requirements

--- bin/install-k0s-web.sh
+++ bin/install-k0s-web.sh
@@ -8,8 +8,22 @@ PYTHON_VERSION="3.7"
 DEST="${HOME}/web3"
 rm -rf "${DEST}"
 
+# Make a directory for k0s.org web
 mkdir -p "${DEST}"
 cd "${DEST}"
-pyenv install "${PYTHON_VERSION}"
+
+# Install the proper version of Python with pyenv
+# See https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+pyenv install --skip-existing "${PYTHON_VERSION}"
 pyenv local "${PYTHON_VERSION}"
 python --version
+
+# Create a virtual environment
+python -m venv .
+
+# Activate the virtual environment
+. bin/activate
+
+# Install the required packages
+bin/pip install --upgrade pip
+bin/pip install -r "${HOME}"/web/requirements.txt
